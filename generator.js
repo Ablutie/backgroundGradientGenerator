a89f86cba3c4b41640ac3445df1cbf3e3a66aa3d
Ablutie <mishu_navetistul@yahoo.com>
2019-11-11 10:54:15 +0200

    added lodash

--- generator.js
+++ generator.js
@@ -1,3 +1,9 @@
+var _ = require("lodash");
+console.log(_);
+
+let array = [1,2,3,4,5,6,7,8];
+console.log(_.without(array, 3));
+
 const leftColor = document.querySelector(".left");
 const rightColor = document.querySelector(".right"); 
 const body = document.getElementById("gradient");
